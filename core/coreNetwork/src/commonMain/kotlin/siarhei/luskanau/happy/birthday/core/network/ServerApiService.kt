0bf30178997eba2e3debe9c82c6f7e1c2461ba7c
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-05-18 14:06:29 +0200

    Recovery WebSocket client connection after the server connection is lost

--- core/coreNetwork/src/commonMain/kotlin/siarhei/luskanau/happy/birthday/core/network/ServerApiService.kt
+++ core/coreNetwork/src/commonMain/kotlin/siarhei/luskanau/happy/birthday/core/network/ServerApiService.kt
@@ -1,10 +1,9 @@
 package siarhei.luskanau.happy.birthday.core.network
 
-import kotlinx.coroutines.flow.Flow
 import siarhei.luskanau.happy.birthday.core.common.model.BirthdayTheme
 
 interface ServerApiService {
-    fun openWebSocket(): Flow<AnniversaryData?>
+    suspend fun listenAnniversaryData(callback: (AnniversaryData?) -> Unit)
 }
 
 data class AnniversaryData(val name: String, val dob: Long, val theme: BirthdayTheme)
--- core/coreNetwork/src/commonMain/kotlin/siarhei/luskanau/happy/birthday/core/network/ktor/KtorServerApiService.kt
+++ core/coreNetwork/src/commonMain/kotlin/siarhei/luskanau/happy/birthday/core/network/ktor/KtorServerApiService.kt
@@ -1,7 +1,5 @@
 package siarhei.luskanau.happy.birthday.core.network.ktor
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import org.koin.core.annotation.Single
 import siarhei.luskanau.happy.birthday.core.common.model.BirthdayTheme
 import siarhei.luskanau.happy.birthday.core.network.AnniversaryData
@@ -11,22 +9,25 @@ import siarhei.luskanau.happy.birthday.core.network.ktor.model.ThemeDto
 @Single
 internal class KtorServerApiService(private val client: ServerApiClient) : ServerApiService {
 
-    override fun openWebSocket(): Flow<AnniversaryData?> = client.openWebSocket()
-        .map { anniversaryDto ->
-            anniversaryDto?.let { dto ->
-                if (dto.name != null && dto.dob != null && dto.theme != null) {
-                    AnniversaryData(
-                        name = dto.name,
-                        dob = dto.dob,
-                        theme = when (dto.theme) {
-                            ThemeDto.ELEPHANT -> BirthdayTheme.ELEPHANT
-                            ThemeDto.FOX -> BirthdayTheme.FOX
-                            ThemeDto.PELICAN -> BirthdayTheme.PELICAN
-                        }
-                    )
-                } else {
-                    null
+    override suspend fun listenAnniversaryData(callback: (AnniversaryData?) -> Unit) {
+        client.listenAnniversaryData { anniversaryDto ->
+            callback.invoke(
+                anniversaryDto?.let { dto ->
+                    if (dto.name != null && dto.dob != null && dto.theme != null) {
+                        AnniversaryData(
+                            name = dto.name,
+                            dob = dto.dob,
+                            theme = when (dto.theme) {
+                                ThemeDto.ELEPHANT -> BirthdayTheme.ELEPHANT
+                                ThemeDto.FOX -> BirthdayTheme.FOX
+                                ThemeDto.PELICAN -> BirthdayTheme.PELICAN
+                            }
+                        )
+                    } else {
+                        null
+                    }
                 }
-            }
+            )
         }
+    }
 }
